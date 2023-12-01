cfbe02625855fcfaddd454a30447e5fb3e7649e4
StudioRandson <randson.farias@gmail.com>
2023-12-01 14:15:44 -0300

    readicionado conexão1

--- avaliacos3/conexao.php
+++ avaliacos3/conexao.php
@@ -2,7 +2,7 @@
 	$servidor = "localhost";
 	$usuario = "root";
 	$senha = "";
-	$dbname = "star1";
+	$dbname = "star2";
 	
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
